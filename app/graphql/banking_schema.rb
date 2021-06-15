0a1abd2e43a3dd900218bced905c1be7d87acbde
Ilia Lobsanov <ilia@nurey.com>
2021-06-15 17:00:49 -0400

    Fix graphql deprecation warnings

--- app/graphql/banking_schema.rb
+++ app/graphql/banking_schema.rb
@@ -2,12 +2,5 @@ class BankingSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 
-  # Opt in to the new runtime (default in future graphql-ruby versions)
-  use GraphQL::Execution::Interpreter
-  use GraphQL::Analysis::AST
-
-  # Add built-in connections for pagination
-  use GraphQL::Pagination::Connections
-
   use GraphQL::Batch
 end
