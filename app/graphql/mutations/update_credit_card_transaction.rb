ef26b1fc22de89717a2af5b9cf4e0fe15d65cb3b
Ilia Lobsanov <ilia@nurey.com>
2021-06-16 21:36:35 -0400

    Save transaction note using nested attributes

--- app/graphql/mutations/update_credit_card_transaction.rb
+++ app/graphql/mutations/update_credit_card_transaction.rb
@@ -2,16 +2,16 @@ module Mutations
   class UpdateCreditCardTransaction < BaseMutation
     null true
 
-    argument :note, String, required: true
+    argument :note_detail, String, required: true
+    argument :note_id, ID, required: false
     argument :id, ID, required: true
 
     field :credit_card_transaction, Types::CreditCardTransactionType, null: true
     field :errors, [String], null: false
 
-    def resolve(note:, id:)
+    def resolve(note_detail:, note_id: nil, id:)
       tx = CreditCardTransaction.find_by!(id: id)
-      tx.note.detail = note
-      if tx.save
+      if tx.update(note_attributes: { detail: note_detail, id: note_id })
         {
           credit_card_transaction: tx,
           errors: []
