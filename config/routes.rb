1d0b27425e6238066c50fbf465c3af923377f971
Ilia Lobsanov <ilia@nurey.com>
2024-04-07 14:36:43 -0400

    Add healthcheck route
    Rails 7.1 introduced built-in health check. Use it.
    This is needed for Kamal.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
   post '/graphql', to: 'graphql#execute'
   resources :credit_card_transactions, only: :index do
     collection do
